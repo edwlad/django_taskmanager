81d7572cdf5983f1edcb7bd575510ea9e08ae73c
edwlad <80802093+edwlad@users.noreply.github.com>
2023-02-03 15:47:05 +0700

    v_0.0.1

---
+++ app_task/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class TaskStatus(models.Model):
+    pass
+
+
+class Tasks(models.Model):
+    pass
+
+
+class ArhTaskStatus(models.Model):
+    pass
+
+
+class ArhTasks(models.Model):
+    pass
